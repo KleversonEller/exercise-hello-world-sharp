ad1446217b664a53427cb144d9fe1fbf06f452a0
Kleverson Eller <kleverson.a16@gmail.com>
2023-02-19 15:39:49 -0300

    test: :white_check_mark: Testanto func "Greet"
    Testando a func "Greet" para ver se retorna a string "Ola, Mundo!"

--- src/ola-mundo.Test/OlaMundoTest.cs
+++ src/ola-mundo.Test/OlaMundoTest.cs
@@ -2,15 +2,17 @@ using ola_mundo;
 using System.IO;
 using Xunit;
 using System;
+using FluentAssertions;
 
 namespace ola_mundo.Test;
 
 public class OlaMundoTest
 {
     [Theory(DisplayName = "Deve exibir Olá, Mundo!")]
-    [InlineData("<Insira algo aqui>")]
+    [InlineData("Olá, Mundo!")]
     public void TestOlaMundo(string Expected)
     {
-        throw new NotImplementedException();
+        var result = HelloWorld.Greet();
+        result.Should().Be(Expected);
     }
 }
